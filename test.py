1af24d9018f258acb1a2f99e4277122f7a250834
njorogetimk <njorogetimk@gmail.com>
2022-05-19 18:17:58 +0300

    Modify test for reverse input order

--- test.py
+++ test.py
@@ -9,10 +9,13 @@ def test_usecase_2() -> None:
     """Add two lists containing zeroes"""
     assert add_2([0], [0]) == [0]
 
-
 def test_usecase_3() -> None:
     """Add two lists of nonequal length"""
     assert add_2([9,9,9,9,9,9,9], [9,9,9,9]) == [8,9,9,9,0,0,0,1]
+
+def test_usecase_3_2() -> None:
+    """Add two lists of nonequal length"""
+    assert add_2([9,9,9,9], [9,9,9,9,9,9,9]) == [8,9,9,9,0,0,0,1]
 
 def test_edgecase_1() -> None:
     """Test for an empty list"""
