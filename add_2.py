0a833adc8278798e8c8edfdc1c478cdb7d498972
njorogetimk <njorogetimk@gmail.com>
2022-05-21 08:40:44 +0300

    push incomplete linked list

--- add_2.py
+++ add_2.py
@@ -1,3 +1,5 @@
+
+"""
 from typing import List
 from collections import deque
 
@@ -20,3 +22,37 @@ def add_2(l1: List[int], l2: List[int]) -> List[int]:
     result = [int(i) for i in list(deque(str(total)))]
     result.reverse()
     return result
+"""
+
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        result = ListNode(0)
+        result_tail = result
+        carry = 0
+                
+        while l1 or l2 or carry:            
+            val1  = (l1.val if l1 else 0)
+            val2  = (l2.val if l2 else 0)
+            carry, out = divmod(val1+val2 + carry, 10)    
+                      
+            result_tail.next = ListNode(out)
+            result_tail = result_tail.next                      
+            
+            l1 = (l1.next if l1 else None)
+            l2 = (l2.next if l2 else None)
+               
+        return result.next
--- test.py
+++ test.py
@@ -1,26 +1,31 @@
-from add_2 import add_2
+from add_2 import ListNode, Solution
 
-def test_usecase_1() -> None:
-    """Add two lists of the same length"""
-    assert add_2([2,4,3], [5,6,4]) == [7,0,8]
+# def test_usecase_1() -> None:
+#     """Add two lists of the same length"""
+#     solve = Solution()
+#     assert solve.addTwoNumbers([2,4,3], [5,6,4]) == [7,0,8]
 
+# def test_usecase_2() -> None:
+#     """Add two lists containing zeroes"""
+#     solve = Solution()
+#     assert solve.addTwoNumbers([0], [0]) == [0]
 
-def test_usecase_2() -> None:
-    """Add two lists containing zeroes"""
-    assert add_2([0], [0]) == [0]
+# def test_usecase_3() -> None:
+#     """Add two lists of nonequal length"""
+#     solve = Solution()
+#     assert solve.addTwoNumbers([9,9,9,9,9,9,9], [9,9,9,9]) == [8,9,9,9,0,0,0,1]
 
-def test_usecase_3() -> None:
-    """Add two lists of nonequal length"""
-    assert add_2([9,9,9,9,9,9,9], [9,9,9,9]) == [8,9,9,9,0,0,0,1]
+# def test_usecase_3_2() -> None:
+#     """Add two lists of nonequal length"""
+#     solve = Solution()
+#     assert solve.addTwoNumbers([9,9,9,9], [9,9,9,9,9,9,9]) == [8,9,9,9,0,0,0,1]
 
-def test_usecase_3_2() -> None:
-    """Add two lists of nonequal length"""
-    assert add_2([9,9,9,9], [9,9,9,9,9,9,9]) == [8,9,9,9,0,0,0,1]
+# def test_edgecase_1() -> None:
+#     """Test for an empty list"""
+#     solve = Solution()
+#     assert solve.addTwoNumbers([], []) == [0]
 
-def test_edgecase_1() -> None:
-    """Test for an empty list"""
-    assert add_2([], []) == [0]
-
-def test_edgecase_2() -> None:
-    """Test single empty list"""
-    assert add_2([], [3]) == [3]
+# def test_edgecase_2() -> None:
+#     """Test single empty list"""
+#     solve = Solution()
+#     assert solve.addTwoNumbers([], [3]) == [3]
