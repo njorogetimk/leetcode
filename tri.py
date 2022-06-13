10996b2e89b0d6936c14883ee0750b47b1dbe63c
njorogetimk <njorogetimk@gmail.com>
2022-06-13 20:38:56 +0300

    complete attempt 1

--- tri.py
+++ tri.py
@@ -2,4 +2,16 @@ from typing import List
 
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        pass
+        
+        for i in range(1, len(triangle)):
+            for j in range(len(triangle[i])):
+                if j == 0:
+                    triangle[i][j] += triangle[i-1][j]
+                elif j == len(triangle[i])-1:
+                    triangle[i][j] += triangle[i-1][j-1]
+                else:
+                    triangle[i][j] += min(triangle[i-1][j-1], triangle[i-1][j])
+                # print(triangle[i],triangle[i-1])
+        
+
+        return min(triangle[len(triangle)-1])
--- tri_test.py
+++ tri_test.py
@@ -12,3 +12,13 @@ def test_use_2() -> None:
     """Input: triangle = [[-10]]"""
 
     assert solve.minimumTotal([[-10]]) == -10
+
+def test_use_3() -> None:
+    """Input: triangle = [[2],[3,4],[6,6,6],[1,3,7,5]]"""
+
+    assert solve.minimumTotal([[2],[3,4],[6,6,6],[1,3,7,5]]) == 12
+
+def test_use_4() -> None:
+    """Input: triangel = [[-1],[2,3],[1,-1,-3]]"""
+
+    assert solve.minimumTotal([[-1],[2,3],[1,-1,-3]]) == -1
