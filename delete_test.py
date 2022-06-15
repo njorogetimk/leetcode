7884c8a5e81291d10c66dafc5e3bf040714be3de
njorogetimk <njorogetimk@gmail.com>
2022-06-15 17:46:47 +0300

    Postpone the task

--- delete_test.py
+++ delete_test.py
@@ -7,7 +7,20 @@ def test_use_1() -> None:
 
     assert solve.minDistance("sea", "eat") == 2
 
+
 def test_use_2() -> None:
     """Input: word1 = "leetcode", word2 = "etco" """
 
     assert solve.minDistance("leetcode", "etco") == 4
+
+
+def test_use_3() -> None:
+    """Input: word1 = "sela", word2 = "eat" """
+
+    assert solve.minDistance("sela", "eat") == 3
+
+
+def test_use_4() -> None:
+    """Input: word1 = "leetocde", word2 = "etco" """
+
+    assert solve.minDistance("leetocde", "etco") == 6
